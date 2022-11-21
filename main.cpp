8d81b59bb4c465e7cf086d741003c26f21f035b9
Kobe <kobechua14@gmail.com>
2022-11-21 17:53:21 +0000

    File input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+int printFile(){
+
+}
+
 int main(){
+    ifstream inputFile("input.txt");
+    
+    string line;
+    if (inputFile.is_open()){
+        while ( getline(inputFile, line)){
+            cout << line << 'n';
+        }
+        inputFile.close();
+    }
+
+
+    inputFile.close();
 
     cout << "Hello World!" << endl;
 
