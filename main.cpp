9e8a7359a86a7c96cb3fe89ead28602918ed2b9a
kobechua <kobechua14@gmail.com>
2022-11-25 17:34:25 -0500

    final draft

--- main.cpp
+++ main.cpp
@@ -2,63 +2,10 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <stack>
 
 using namespace std;
 
-//DEBUG FUNCTIONS
-
-void printFile(ifstream &file){
-
-    string line;
-    if (file.is_open()){
-        while ( getline(file, line)){
-            cout << line << '\n';
-        }
-        file.close();
-    }
-
-}
-
-void printFormat(vector<vector<int>> format){
-    for (int a = 0;a < 3; a++){
-        for (int b = 0; b < 3; b++){
-            cout << format[b][a] << " ";
-        }
-        cout << endl;
-    }
-}
-
-void printMaze(vector<vector<vector<string>>> maze){
-        for (int k = 0; k < 5; k++){
-            for (int j = 0; j < 4; j++){
-                for (int i = 0; i < 4; i++){
-                    cout << maze[k][i][j] << " ";
-                }
-                cout << endl;
-            }    
-            cout << endl;
-        }
-}
-
-//
-
-class Maze{
-    vector<int> dimensions;
-    vector<int> start;
-    vector<int> goal;
-
-    vector<int> current;
-
-    vector<vector<int>> getFormat(ifstream& file);
-
-    vector<vector<vector<string>>> initializeMaze(ifstream& file, vector<int>& dimensions, vector<int>& start, vector<int>& goal);
-
-    void moveCurrent(int move, vector<int>& current);
-
-};
-
-//Gets formatting 3x3 input
+//Gets the maze formatting 3x3 input
 vector<vector<int>> getFormat(ifstream& file){
     vector<vector<int>> format(3, vector<int>(3)); 
     int number = 0;
@@ -69,8 +16,6 @@ vector<vector<int>> getFormat(ifstream& file){
             format[i][j] = number;
         }
     }
-
-
     return format;
 }
 
@@ -84,14 +29,13 @@ vector<vector<vector<string>>> initializeMaze(ifstream& file, vector<vector<int>
             for (int i = 0; i < format[1][0]; i++){
                 file >> number;
                 maze[k][i][j] = number;
-
             }
         }
     }
     return maze;
 }
 
-//Get next move indices of maze cell
+//Gets the indices of all the cell's possible next moves
 vector<int> getIndices(string input){
     vector<int> indices;
     for(size_t i = 0; i < input.size(); i++){
@@ -102,33 +46,7 @@ vector<int> getIndices(string input){
     return indices;
 }
 
-//move current maze based on input "move" and current location
-vector<int> moveCurrent(int move, vector<int> current){
-    vector<int> destination;
-    cout << "Moving: " << current[0] << ", " << current[1] << ", " << current[2] << endl;
-    if(move == 0){
-        current[2] -= 1;
-    }
-    else if(move == 1){
-        current[1] += 1;
-    }
-    else if(move == 2){
-        current[2] += 1;
-    }
-    else if(move == 3){
-        current[1] -= 1;
-    }
-    else if(move == 4){
-        current[0] += 1;
-    }
-    else if(move == 5){
-        current[0] -= 1;
-    }
-    cout << "Moved to: " << current[0] << ", " << current[1] << ", " << current[2]<< "\n" <<endl;
-    destination = current;
-    return destination;
-}
-
+//This converts the move/direction to its associated character counterpart
 string convertMove(int move){
     if(move == 0){
         return "N";
@@ -151,90 +69,77 @@ string convertMove(int move){
     else return ":(";
 }
 
-//find path from start coordinates to goal coordinates
-vector<string> solve(vector<vector<int>> format, vector<vector<vector<string>>> maze){
-    vector<int> current(3);
-    vector<int> start(3);
-    vector<int> goal(3);
-
-    vector<vector<vector<int>>> discovered(format[0][0], vector<vector<int>>(format[1][0],vector<int>(format[2][0], 0)));
-
-
-
-    vector<string> answer(format[0][0]*format[1][0]*format[2][0]);
-
-
-    for (int i = 0; i < 3; i++){
-        current[i] = format[i][1];
-        start[i] = format[i][1];
-        goal[i] = format[i][2];
+//move the "spider" based on input move and current cell location
+vector<int> moveCurrent(int move, vector<int> current){
+    if(move == 0){
+        current[2] -= 1;
     }
-
-    stack<vector<int>> childrenStack;
-    
-    childrenStack.push(current);
-
-    int totalmoves = 0;
-
-    while (!childrenStack.empty()){
-        vector<int> cur = childrenStack.top();
-        childrenStack.pop();
-
-        
-
-        vector<int> children = getIndices(maze[cur[0]][cur[1]][cur[2]]);
-
-
-        if (cur == goal){
-            break;
-        }
-
-        if(discovered[cur[0]][cur[1]][cur[2]] == 0){
-            discovered[cur[0]][cur[1]][cur[2]] = 1;
-         }
-        // for (size_t i = 0; i < children.size(); i++){
-        for (size_t i = children.size(); i > 0; --i){
-            vector<int> child = moveCurrent(children[i-1], cur);
-            if (discovered[child[0]][child[1]][child[2]] == 0){
-                totalmoves += 1;
-                answer.emplace_back(convertMove(children[i-1]));
-                childrenStack.push(child);
-            }
-        }
+    else if(move == 1){
+        current[1] += 1;
     }
-    cout << totalmoves << endl;
-    return answer;
+    else if(move == 2){
+        current[2] += 1;
+    }
+    else if(move == 3){
+        current[1] -= 1;
+    }
+    else if(move == 4){
+        current[0] += 1;
+    }
+    else if(move == 5){
+        current[0] -= 1;
+    }
+    return current;
 }
 
-//read file, run solve, return output
+//Recursive DFS + Backtracking function, returns 1 as true and 0 as false. Backtracks when a call returns false.
+int DFS(vector<int> current, vector<vector<vector<string>>> maze, vector<int> goal, vector<vector<vector<int>>>& discovered, vector<string>& path, string moveused){
+    
+    discovered[current[0]][current[1]][current[2]] = 1;
+    
+    path.push_back(moveused);
+
+    if (current == goal){
+        return 1;
+    }
+
+    vector<int> children = getIndices(maze[current[0]][current[1]][current[2]]);
+
+    for(size_t i = 0; i < children.size(); i++){
+        vector<int> child = moveCurrent(children[i], current);
+        if (discovered[child[0]][child[1]][child[2]] != 1){
+            if (DFS(child, maze, goal, discovered, path, convertMove(children[i])) == 1) return 1;
+        }
+    }
+    path.pop_back();
+    return 0;
+}
+
+//Driver code, opens files, initializes the maze and its format, calls the DFS functions and puts the output in the output.txt file
 int main(){
 
     ifstream file("input.txt");
 
     vector<vector<int>> format = getFormat(file);
-
-    printFormat(format);
-
     vector<vector<vector<string>>> maze = initializeMaze(file, format);
+
+    vector<int> start = {format[0][1],format[1][1], format[2][1]};
+    vector<int> goal = {format[0][2],format[1][2], format[2][2]};
+    vector<vector<vector<int>>> discovered(format[0][0], vector<vector<int>>(format[1][0],vector<int>(format[2][0], 0)));
     
-    printMaze(maze);
+    vector<string> answer;
 
-    // vector<int> current(3);
-
-    // vector<int> indices = getIndices("010001"); 
-
-    // for (size_t i = 0; i < indices.size(); i++){
-    //     cout << indices[i] << endl;
-    // }
-
-    vector<string> answer = solve(format, maze);
-
-    for (size_t i = 0; i < answer.size(); i++){
-        cout << answer[i];
-    }
-    
+    DFS(start, maze, goal, discovered, answer, "");
 
     file.close();
+
+    ofstream output("output.txt");
+
+    for (size_t i = 1; i < answer.size(); i++){
+        output << answer[i] << " ";
+    }
+
+    output.close();
 
     return 0;
 }
